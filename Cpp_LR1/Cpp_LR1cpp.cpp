8122cd236e401adc7b816b2d1a57013db78f58f2
Dara <moskalenko.dara@mail.ru>
2022-09-19 20:03:40 +0300

    изменено пять тыщ раз

--- Cpp_LR1/Cpp_LR1cpp.cpp
+++ Cpp_LR1/Cpp_LR1cpp.cpp
@@ -7,41 +7,49 @@ using namespace std;
 *fill array with integers
 * 
 * @param a array with integers
-* @param N constant
+* @param N max number up to which need to find all prime numbers
 */
 void fill(int* a, const int N);
+
 /*
 *Eratosthenes algorithm
 * 
 * @param a array with integers
-* @param N constant
+* @param N max number up to which need to find all prime numbers
 */
-void func_for_prime(int* a, const int N);
+void func_eratosthenes(int* a, const int N);
+
 /*
 *output array on console
 *
 * @param a array with prime numbers
-* @param N constant
+* @param N max number up to which need to find all prime numbers
 */
 void print(int* a, const int N);
 
+/*
+*main function for declaring variables and calling functions
+* 
+* @return returns 0 if the program completed successfully
+*/
 int main() 
 {
 	const int N = 30;
 	int* a = new int[N];
 	fill(a, N);
-	func_for_prime(a, N);
+	func_eratosthenes(a, N);
 	print(a, N);
 	delete[] a;
 
 	cin.get();
+	return 0;
 }
 void fill(int* a, const int N) {
 	//fill array with integers
 	for (int i = 0; i < N; i++)
 		a[i] = i;
 }
-void func_for_prime(int* a, const int N)
+void func_eratosthenes(int* a, const int N)
 {
 	//0 and 1 aren't prime numbers
 	a[0] = a[1] = 0;
