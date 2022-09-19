04583005da1737411ff3ce9fcad44c328a348723
Dara <moskalenko.dara@mail.ru>
2022-09-19 12:50:46 +0300

    изменено 3

--- Cpp_LR1/Cpp_LR1cpp.cpp
+++ Cpp_LR1/Cpp_LR1cpp.cpp
@@ -5,21 +5,25 @@ using namespace std;
 
 int main() 
 {
-	int const A = 1001;
+	const int A = 1001;
 	int prime[A];
+
 	//fill array with integers
-	for (int i = 0; i < A; i++) {
+	for (int i = 0; i < A; i++)
 		prime[i] = i;
 
-	}
+	//0 and 1 aren't prime numbers
+	prime[0] = prime[1] = 0; 
 
-	prime[0] = prime[1] = 0; //0 and 1 aren't prime numbers
-
-	for (int i = 2; i * i < A; i++)
-		if (prime[i]) {
-			for (int j = i * i; j < A; j += i) //reset all the numbers that are multiples of the number "i"
+	for (int i = 2; i * i < A; i++) 
+	{
+		if (prime[i]) 
+		{
+			//reset all the numbers that are multiples of the number "i"
+			for (int j = i * i; j < A; j += i) 
 				prime[j] = 0;
 		}
+	}
 
 	//output array on console
 	for (int i = 0; i < A; i++)
